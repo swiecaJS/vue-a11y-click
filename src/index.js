eeb8c14cc36523998dfb329d29c996c9d1400c73
swiecaJS <karol.swieca@netguru.co>
2019-05-01 12:01:48 +0200

    initial directive code

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const a11yClick = {
+  install(Vue) {
+    Vue.directive("a11y-click", {
+      bind(el) {
+        let pressed = false;
+
+        el.tabIndex = 0;
+        el.setAttribute("role", "button");
+        el.setAttribute("aria-pressed", "false");
+
+        el.addEventListener("keydown", () => {
+          // enter keycode = 13 || space keycode = 32
+          if (event.keyCode !== 13 || event.keyCode !== 32 || pressed) return;
+          pressed = true;
+          el.setAttribute("aria-pressed", "true");
+          el.click();
+        });
+
+        el.addEventListener("keyup", () => {
+          if (event.keyCode !== 13) return;
+          pressed = false;
+          el.setAttribute("aria-pressed", "false");
+        });
+      },
+      unbind(el) {
+        el.removeEventListener("keyup");
+      }
+    });
+  }
+};
+
+// auto install
+if (typeof window !== "undefined" && typeof window.Vue !== "undefined") {
+  window.Vue.use(a11yClick);
+}
+
+export default a11yClick;
