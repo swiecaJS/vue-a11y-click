c79f8d1a397f8218bde8179dafd7911a1bf247d6
swiecaJS <karol.swieca@netguru.co>
2019-05-01 12:46:50 +0200

    fixed unbinding event listeners

--- src/index.js
+++ src/index.js
@@ -1,29 +1,35 @@
 const a11yClick = {
   install(Vue) {
+    let handleKeyUp, handleKeyDown;
+
     Vue.directive("a11y-click", {
       bind(el) {
         let pressed = false;
 
-        el.tabIndex = 0;
-        el.setAttribute("role", "button");
-        el.setAttribute("aria-pressed", "false");
-
-        el.addEventListener("keydown", () => {
+        handleKeyDown = () => {
           // enter keycode = 13
           if (event.keyCode !== 13 || pressed) return;
           pressed = true;
           el.setAttribute("aria-pressed", "true");
           el.click();
-        });
+        };
 
-        el.addEventListener("keyup", () => {
+        handleKeyUp = () => {
           if (event.keyCode !== 13) return;
           pressed = false;
           el.setAttribute("aria-pressed", "false");
-        });
+        };
+
+        el.tabIndex = 0;
+        el.setAttribute("role", "button");
+        el.setAttribute("aria-pressed", "false");
+
+        el.addEventListener("keydown", handleKeyDown);
+        el.addEventListener("keyup", handleKeyUp);
       },
       unbind(el) {
-        el.removeEventListener("keyup");
+        el.removeEventListener("keyup", handleKeyUp);
+        el.removeEventListener("keydown", handleKeyDown);
       }
     });
   }
